297aea191f6021b55a11b669d6b4027fdded5abd
梁娅勤 <liangyaqin@bytedance.com>
2023-07-09 00:46:25 +0800

    fix: redirect query

--- src/app/auth/route.ts
+++ src/app/auth/route.ts
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
+import { NextRequest } from 'next/server';
 
 // 平台配置的接口, 临时转发
-export async function GET() {
-  redirect('/api/auth');
+export async function GET(request: NextRequest) {
+  redirect('/api/auth?' + request.nextUrl.searchParams.toString());
 }
